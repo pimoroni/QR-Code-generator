7e6809df5a9fe418333594f0b3d464e9a238faf1
Phil Howard <phil@gadgetoid.com>
2022-09-27 13:32:21 +0100

    MicroPython: Fix whitespace errors.

--- micropython/qrcode.c
+++ micropython/qrcode.c
@@ -3,15 +3,15 @@
 
 typedef struct _qrcode_obj_t {
     mp_obj_base_t base;
-	uint8_t *qr0;
-	uint8_t *tempBuffer;
+    uint8_t *qr0;
+    uint8_t *tempBuffer;
     enum qrcodegen_Ecc ecc;
     enum qrcodegen_Mask mask;
 } qrcode_obj_t;
 
 mp_obj_t qrcode___del__(mp_obj_t self_in) {
     qrcode_obj_t *self = MP_OBJ_TO_PTR(self_in);
-	(void)self;
+    (void)self;
     return mp_const_none;
 }
 
@@ -28,7 +28,7 @@ mp_obj_t qrcode_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, 
 
     self->base.type = &qrcode_type;
     self->qr0 = m_new(uint8_t, qrcodegen_BUFFER_LEN_MAX);
-	self->tempBuffer = m_new(uint8_t, qrcodegen_BUFFER_LEN_MAX);
+    self->tempBuffer = m_new(uint8_t, qrcodegen_BUFFER_LEN_MAX);
     self->ecc = args[ARG_ecc].u_int;
     self->mask = args[ARG_mask].u_int;
 
@@ -39,33 +39,33 @@ mp_obj_t qrcode_set_text(mp_obj_t self_in, mp_obj_t text) {
     qrcode_obj_t *self = MP_OBJ_TO_PTR(self_in);
 
     if(mp_obj_is_str_or_bytes(text)) {
-		// Defines str in-macro and populates it with the string contents
-		// null-terminated
-		GET_STR_DATA_LEN(text, str, str_len);
+        // Defines str in-macro and populates it with the string contents
+        // null-terminated
+        GET_STR_DATA_LEN(text, str, str_len);
 
-		bool ok = qrcodegen_encodeText((const char*)str,
-			self->tempBuffer, self->qr0, self->ecc,
-			qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX,
-			self->mask, true);
+        bool ok = qrcodegen_encodeText((const char*)str,
+            self->tempBuffer, self->qr0, self->ecc,
+            qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX,
+            self->mask, true);
 
-		return ok ? mp_const_true : mp_const_false;
+        return ok ? mp_const_true : mp_const_false;
     }
 
-	return mp_const_false;
+    return mp_const_false;
 }
 
 mp_obj_t qrcode_get_module(mp_obj_t self_in, mp_obj_t x, mp_obj_t y) {
     qrcode_obj_t *self = MP_OBJ_TO_PTR(self_in);
 
-	return qrcodegen_getModule(self->qr0, mp_obj_get_int(x), mp_obj_get_int(y)) ? mp_const_true : mp_const_false;
+    return qrcodegen_getModule(self->qr0, mp_obj_get_int(x), mp_obj_get_int(y)) ? mp_const_true : mp_const_false;
 }
 
 mp_obj_t qrcode_get_size(mp_obj_t self_in) {
     qrcode_obj_t *self = MP_OBJ_TO_PTR(self_in);
 
-	mp_obj_t size = mp_obj_new_int(qrcodegen_getSize(self->qr0));
+    mp_obj_t size = mp_obj_new_int(qrcodegen_getSize(self->qr0));
 
-	mp_obj_t tuple[2];
+    mp_obj_t tuple[2];
     tuple[0] = size;
     tuple[1] = size;
     return mp_obj_new_tuple(2, tuple);
@@ -116,7 +116,7 @@ const mp_obj_type_t qrcode_type = {
 #endif
 
 STATIC const mp_map_elem_t qrcode_globals_table[] = {
-	{ MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_qrcode) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_qrcode) },
     { MP_OBJ_NEW_QSTR(MP_QSTR_QRCode), (mp_obj_t)&qrcode_type },
 
     { MP_ROM_QSTR(MP_QSTR_ECC_LOW), MP_ROM_INT(qrcodegen_Ecc_LOW) },
@@ -144,8 +144,8 @@ STATIC const mp_map_elem_t qrcode_globals_table[] = {
 STATIC MP_DEFINE_CONST_DICT(mp_module_qrcode_globals, qrcode_globals_table);
 
 const mp_obj_module_t qrcode_user_cmodule = {
-	.base = { &mp_type_module },
-	.globals = (mp_obj_dict_t*)&mp_module_qrcode_globals,
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t*)&mp_module_qrcode_globals,
 };
 
 #if MICROPY_VERSION <= 70144
